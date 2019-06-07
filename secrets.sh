6cee7b1e2d7ac31bf1e2b204e3dcc6470426930d
paul <pauls@windocks.com>
2019-06-07 15:10:45 -0700

    first commit

---
+++ secrets.sh
@@ -0,0 +1,2 @@
+kubectl create secret generic proxy-secrets --from-literal=WINDOCKS_REQUIRED_USERNAME='administrator' --from-literal=WINDOCKS_REQUIRED_PASSWORD='placeholder123' --from-literal=WINDOCKS_REQUIRED_CONTAINER_SAPASSWORD='yat!834sd'
+
